c7d09723d4d0ea144c72e84d2babda4f97d1f5a1
sujatha10 <muvva.sujatha1@gmail.com>
2024-08-15 15:42:09 +0530

    first commit

---
+++ src/app/Register/Register.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { useState } from "react"
+
+export const Register = () =>{
+    const [data,setData] = useState({})
+
+    const fnRegister = async () =>{
+        try {
+        var dataObj = {
+            "data":data
+        }
+
+        const res = await fetch ("http://localhost:2020/std/register",{
+            method : 'post',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(dataObj)
+        })
+        const result = await res.json()
+        console.log(result)
+    } catch (ex) {
+        console.error(ex);
+    }
+    }
+    const handleChange = (eve:any) => {
+            const {name,value} = eve.target
+            setData({...data,[name]:value})
+    }
+    return (
+        <div>
+            <h3>Register</h3>
+            <p>
+                <b>Name:</b><input name="name" onChange={handleChange}></input>
+            </p>
+            <p>
+                <b>Rno:</b><input type='number' name='rno' onChange={handleChange}/>
+            </p>
+            <p>
+                <b>Location:</b><textarea name='loc' onChange={handleChange}/>
+            </p>
+            <p>
+                <button onClick={fnRegister}>register</button>
+            </p>
+        </div>
+    )
+}
---
+++ typescript/Arth.ts
@@ -0,0 +1,7 @@
+class Arth {
+    n1:number = 20 ;
+    n2:number = 10;
+    add() {
+        console.log(this.n1 + this.n2)
+    }
+}
